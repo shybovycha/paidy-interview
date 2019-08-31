ea38da0e6954f22ef5a1bf95bd8593638e59257d
Artem Shubovych <ashubovych@atlassian.com>
2019-08-31 16:46:19 +1000

    Add tests for OneForgeLiveClient

--- forex-mtl/src/test/scala/forex/services/rates/oneforge/cache/OneForgeLiveClientTest.scala
+++ forex-mtl/src/test/scala/forex/services/rates/oneforge/cache/OneForgeLiveClientTest.scala
@@ -1,34 +1,135 @@
 package forex.services.rates.oneforge.cache
 
-import cats.effect.Concurrent
+import java.net.ConnectException
+
+import cats.MonadError
 import cats.effect.IO
-import cats.syntax.all._
+import forex.config.ForexConfig
+import forex.domain._
+import forex.services.rates.Errors
+import forex.services.rates.Errors.Error._
+import org.http4s.InvalidMessageBodyFailure
+import org.http4s.Status
+import org.http4s.Uri
+import org.http4s.client.UnexpectedStatus
 import org.scalatest.FunSuite
-import org.scalatest.Ignore
 import org.scalatest.Matchers._
+
+import scala.concurrent.TimeoutException
+import scala.concurrent.duration._
 
 class OneForgeLiveClientTest extends FunSuite {
 
-  test("#fetchPossiblePairs returns all currency pairs available") {}
+  implicit val me: MonadError[IO, Errors.Error] = ???
 
-  test("#fetchQuotes converts all the currencies passed") {}
+  val config = ForexConfig(host = "http://example.com", apiKey = "TEST_API_KEY", dataExpiresIn = 5.minutes)
 
-  ignore("not testing #oneForgeConvertRate since this would only test http4s client which we do not want to test ourselves")
+  test("#fetchPossiblePairs returns all currency pairs available") {
+    val client = new OneForgeLiveClient[IO](config)
 
-  ignore("not testing #oneForgeSymbols since this would only test http4s client which we do not want to test ourselves")
+    val fetcher = (_: Uri) => IO(List("AUDJPY", "EURUSD"))
 
-  test("#convertRateUri constructs URI from configuration passed") {}
+    client.fetchPossiblePairs(fetcher).unsafeRunSync() should contain only (Rate.Pair(Currency.AUD, Currency.JPY), Rate.Pair(Currency.EUR, Currency.USD))
+  }
 
-  test("#convertRateUri returns CanNotParseConvertUri when unable to parse URI") {}
+  test("#fetchQuotes converts all the currencies passed") {
+    val client = new OneForgeLiveClient[IO](config)
 
-  test("#symbolsUri constructs URI from configuration passed") {}
+    val fetcher = (_: Uri) => IO(List(QuoteDTO("AUDJPY", 700.0)))
+    val currencyPairs = List(Rate.Pair(Currency.AUD, Currency.JPY))
 
-  test("#symbolsUri returns CanNotParseSymbolsUri when unable to parse URI") {}
+    // TODO: this will be updated at convert time and thus might fail / flake
+    val timestamp = Timestamp.now
 
-  test("#quoteToRate extracts Rate from QuoteDTO response object") {}
+    client.fetchQuotes(currencyPairs, fetcher).unsafeRunSync() should contain only (Rate(Rate.Pair(Currency.AUD, Currency.JPY), Price(70.0), timestamp))
+  }
 
-  test("#parseCurrencyPairFromCode extracts pair of currencies from string response") {}
+  ignore("not testing #oneForgeConvertRate since this would only test http4s client which we do not want to test ourselves") {}
 
-  test("#handleHttpError converts network exceptions to errors") {}
+  ignore("not testing #oneForgeSymbols since this would only test http4s client which we do not want to test ourselves") {}
+
+  test("#convertRateUri constructs URI from configuration passed") {
+    val client = new OneForgeLiveClient[IO](config)
+
+    val currencyPairs = List(Rate.Pair(Currency.AUD, Currency.JPY))
+
+    client.convertRateUri(currencyPairs).unsafeRunSync() should be ("http://example.com/convert?pairs=AUDJPY&api_key=TEST_API_KEY")
+  }
+
+  test("#convertRateUri returns CanNotParseConvertUri when unable to parse URI") {
+    val brokenConfig = ForexConfig(host = "123", apiKey = "?api?key", dataExpiresIn = 1.second)
+    val client = new OneForgeLiveClient[IO](brokenConfig)
+
+    val currencyPairs = List(Rate.Pair(Currency.AUD, Currency.JPY))
+
+    // TODO: check the MonadError rather than `not be`
+    client.convertRateUri(currencyPairs).unsafeRunSync() should not be ("123/convert?pairs=AUDJPY&api_key=?api?key")
+  }
+
+  test("#symbolsUri constructs URI from configuration passed") {
+    val client = new OneForgeLiveClient[IO](config)
+
+    client.symbolsUri.unsafeRunSync() should be ("http://example.com/symbols?api_key=TEST_API_KEY")
+  }
+
+  test("#symbolsUri returns CanNotParseSymbolsUri when unable to parse URI") {
+    val brokenConfig = ForexConfig(host = "123", apiKey = "?api?key", dataExpiresIn = 1.second)
+    val client = new OneForgeLiveClient[IO](brokenConfig)
+
+    // TODO: check the MonadError rather than `not be`
+    client.symbolsUri.unsafeRunSync() should not be ("123/convert?api_key=?api?key")
+  }
+
+  test("#quoteToRate extracts Rate from QuoteDTO response object") {
+    val client = new OneForgeLiveClient[IO](config)
+
+    // TODO: this will be updated at convert time and thus might fail / flake
+    val timestamp = Timestamp.now
+
+    client.quoteToRate(QuoteDTO(symbol = "AUDJPY", price = 71.0)) should be (Rate(Rate.Pair(Currency.AUD, Currency.JPY), Price(71.0), timestamp))
+  }
+
+  test("#parseCurrencyPairFromCode extracts pair of currencies from string response") {
+    val client = new OneForgeLiveClient[IO](config)
+
+    client.parseCurrencyPairFromCode("JPYUSD") should be (Rate.Pair(Currency.JPY, Currency.USD))
+  }
+
+  ignore("#parseCurrencyPairFromCode returns error when unable to parse response string") {
+    // val client = new OneForgeLiveClient[IO](config)
+
+    // TODO: what should it be then?
+    // client.parseCurrencyPairFromCode("123456") should not be isInstanceOf[Rate.Pair]
+  }
+
+  test("#handleHttpError converts timeout exception to error") {
+    val exception = new TimeoutException()
+
+    OneForgeLiveClient.handleHttpError(exception) should be (NetworkFailure(exception))
+  }
+
+  test("#handleHttpError converts connect exception to error") {
+    val exception = new ConnectException()
+
+    OneForgeLiveClient.handleHttpError(exception) should be (NetworkFailure(exception))
+  }
+
+  test("#handleHttpError converts invalid message body exception to error") {
+    val exception = InvalidMessageBodyFailure("details")
+
+    OneForgeLiveClient.handleHttpError(exception) should be (BadResponseFailure(exception))
+  }
+
+  test("#handleHttpError converts unexpected status exception to error") {
+    val exception = UnexpectedStatus(Status.Gone)
+
+    OneForgeLiveClient.handleHttpError(exception) should be (BadResponseFailure(exception))
+  }
+
+  test("#handleHttpError converts any other exception to error") {
+    val exception = new RuntimeException("details")
+
+    OneForgeLiveClient.handleHttpError(exception) should be (UnknownFailure(exception))
+  }
 
 }

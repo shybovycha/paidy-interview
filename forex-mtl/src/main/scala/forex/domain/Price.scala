b08a7b6064e2f00a89b7cb1983cb1a1844c2b897
Artem Shubovych <ashubovych@atlassian.com>
2019-08-31 19:32:14 +1000

    Add possibility for Price to be constructed with Double

--- forex-mtl/src/main/scala/forex/domain/Price.scala
+++ forex-mtl/src/main/scala/forex/domain/Price.scala
@@ -5,4 +5,7 @@ case class Price(value: BigDecimal) extends AnyVal
 object Price {
   def apply(value: Integer): Price =
     Price(BigDecimal(value))
+
+  def apply(value: Double): Price =
+    Price(BigDecimal(value))
 }
